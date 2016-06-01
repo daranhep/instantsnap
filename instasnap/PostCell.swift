f4c18c113a8911ee73a3cdb5f3f5ee00931470a8
Dara Nhep <daranhep@Daras-iMac.local>
2016-06-01 12:00:06 -0500

    tableview cell for feed working properly

---
+++ instasnap/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  instasnap
+//
+//  Created by Dara Nhep on 6/1/16.
+//  Copyright © 2016 Dara Nhep. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
