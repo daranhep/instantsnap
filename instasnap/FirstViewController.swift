8af9e5040e5a5601f0d5006c63ed3b5be231217e
Dara Nhep <daranhep@Daras-iMac.local>
2016-06-01 12:00:51 -0500

    added tableviewcell

--- instasnap/FirstViewController.swift
+++ instasnap/FirstViewController.swift
@@ -9,23 +9,39 @@
 import UIKit
 import FirebaseAuth
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func signOutBtnPressed(sender: AnyObject) {
-        
-        try! FIRAuth.auth()!.signOut()
-        performSegueWithIdentifier("signedOut", sender: nil )
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostCell
+    }
+    
+    
+
+
 
 }
 
